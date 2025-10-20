fd6f97e2d32477ecce9037720a041292e23e11eb
Baruch Sheffi <baruch@metatron.co.il>
2025-10-20 14:42:16 +0300

    new client version

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use App\Services\UserService;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
 use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 
 /**
  * @codeCoverageIgnore
@@ -51,36 +51,16 @@ class AuthController extends ParentController
 
     public function register(Request $request)
     {
-        // $credentials = $request->only('token', 'name', 'email', 'district', 'password', 'password_confirmation');
-        // $validator = Validator::make($credentials, [
-        //     'email' => 'bail|required|email|max:255|unique:users',
-        //     'name' => 'bail|required|string|max:255',
-        //     'district' => 'bail|required|string|max:255',
-        //     'password' => 'bail|required|confirmed|min:8|max:16',
-        // ]);
-        $credentials = $request->only('token', 'name', 'workerNumber');
-        $sanitizedName = str_replace([' ', "'", '"'], ['', '', ''], strtolower($credentials['name']));
-        $credentials['email'] = $sanitizedName . $credentials['workerNumber'] . '@someelbitnonexists.com';
-        $credentials['password'] = 'default_password';
-        $validator = Validator::make($credentials, [
-            'name' => 'bail|required|string|max:255',
-            'workerNumber' => 'bail|required|integer|digits:3',
-            'token' => 'required|string',
-            'email' => 'email|max:255|unique:users,email',
-        ],
-            [
-                'workerNumber.required' => 'מספר עובד הינו שדה חובה',
-                'workerNumber.integer' => 'מספר עובד חייב להיות מספר שלם',
-                'workerNumber.digits' => 'מספר עובד חייב להיות באורך 3 ספרות',
-                'email.unique' => 'משתמש כבר רשום במערכת',
-                'email.email' => 'הראה שגיאה עם זיהוי משתמש',
-                'email.max' => 'הראה שגיאה עם זיהוי משתמש',
-            ]
-        );
+        $credentials = $request->only('token', 'name', 'email', 'password', 'password_confirmation');
 
+        $validator = Validator::make($credentials, [
+            'email' => 'bail|required|email|max:255|unique:users',
+            'name' => 'bail|required|string|max:255',
+            'password' => 'bail|required|confirmed|min:8|max:16',
+        ]);
 
         if ($validator->fails()) {
-            $errorLog = $request->only('name', 'workerNumber');
+            $errorLog = $request->only('name', 'email');
             Log::warning('register failed req:' . json_encode($errorLog) . ' errors: ' . json_encode($validator->errors()));
             $error = $validator->errors();
             return $this->respondWithError(array_reverse($error->toArray()));
@@ -91,16 +71,24 @@ class AuthController extends ParentController
             return $this->respondWithError(['error' => $error], 422);
         }
 
+        // if (
+        //     (!preg_match('/^.*meuhedet\.co\.il$/im', $credentials['email'])) &&
+        //     (!preg_match('/^.*metatron\.co.il$/im', $credentials['email'])) &&
+        //     (!preg_match('/^.*chamizer.*$/im', $credentials['email']))
+        // ) {
+        //     $errorLog = $request->only('name', 'email', 'district', 'unit', 'department', 'other');
+        //     Log::warning('register1.5 failed req:' . json_encode($errorLog));
+        //     return $this->respondWithError(['email' => ["כתובת האימייל אינה מורשת להשתתף"]]);
+        // }
         try {
             $credentials['ip_address'] = $request->ip();
             $credentials['is_verified'] = 1; // remove this line when email verification is implemented
-            $user = $this->service->add($credentials);
-            $credentials = $user->only('email');
-            $credentials['password'] = 'default_password'; // default password for login
+            $this->service->add($credentials);
+            $credentials = $request->only('email', 'password');
             $token = $this->service->login($credentials);
             return $this->respondWithData(['token' => $token]);
         } catch (\Exception $e) {
-            $errorLog = $request->only('name', 'workerNumber');
+            $errorLog = $request->only('name', 'email', 'district', 'unit', 'department', 'other');
             Log::warning('register2 failed req:' . json_encode($errorLog) . ' errors: ' . json_encode($e->getMessage()));
             return $this->respondWithError($e->getMessage());
         }
@@ -108,60 +96,27 @@ class AuthController extends ParentController
 
     public function login(Request $request)
     {
-        $credentials = $request->only('token', 'workerNumber', 'name', 'email', 'password');
-        if (isset($credentials['workerNumber'])) {
-            $sanitizedName = str_replace([' ', "'", '"'], ['', '', ''], strtolower($request->input('name')));
-            $credentials['email'] = $sanitizedName . $credentials['workerNumber'] . '@someelbitnonexists.com';
-            $credentials['password'] = 'default_password';
-            $validator = Validator::make($credentials, [
-                'workerNumber' => 'bail|required|integer|digits:3',
-                'name' => 'bail|required|string|max:255',
-                'token' => 'required|string',
-                'email' => 'email|max:255',
-            ],
-                [
-                    'workerNumber.required' => 'מספר עובד הינו שדה חובה',
-                    'workerNumber.integer' => 'מספר עובד חייב להיות מספר שלם',
-                    'workerNumber.digits' => 'מספר עובד חייב להיות באורך 3 ספרות',
-                    'email.email' => 'הראה שגיאה עם זיהוי משתמש',
-                    'email.max' => 'הראה שגיאה עם זיהוי משתמש',
-                ]
-            );
-        } else {
-            // admin logins with email and password
-            $credentials['email'] = strtolower($request->input('email'));
-            $credentials['password'] = $request->input('password');
-            $validator = Validator::make($credentials, [
-                'email' => 'bail|required|string|email',
-                'password' => 'bail|required|string',
-                'token' => 'required|string',
-            ]);
-        }
-
-        // $validator = Validator::make($request->only('email', 'password'), [
-        //     'email' => 'required|string|email',
-        //     'password' => 'required',
-        // ]);
+        $validator = Validator::make($request->only('email', 'password'), [
+            'email' => 'required|string|email',
+            'password' => 'required',
+        ]);
         if ($validator->fails()) {
-            Log::warning('login1 failed req:' . json_encode($credentials['email']) . ' errors: ' . json_encode($validator->errors()));
+            Log::warning('login1 failed req:' . json_encode($request->email) . ' errors: ' . json_encode($validator->errors()));
             return $this->respondWithError($validator->errors());
         }
 
         if (!$this->reCapchaTest($request->input('token') , $request->ip())) {
-            Log::warning('login2 failed req:' . json_encode($credentials['email']. ' errors: תקלה בבדיקת בוטים, נסו שוב בעוד מספר דקות.'));
+            Log::warning('login2 failed req:' . json_encode($request->email));
             $error[0] = 'תקלה בבדיקת בוטים, נסו שוב בעוד מספר דקות.';
             return $this->respondWithError(['error' => $error], 422);
         }
 
-        // $credentials = $request->only('email', 'password');
-        $loginCredentials = [
-            'email' => $credentials['email'],
-            'password' => $credentials['password'],
-        ];
+        $credentials = $request->only('email', 'password');
+
         try {
-            $token = $this->service->login($loginCredentials);
+            $token = $this->service->login($credentials);
         } catch (\Exception $e) {
-            Log::warning('login3 failed req:' . json_encode($loginCredentials['email']) . ' errors: ' . json_encode($e->getMessage()));
+            Log::warning('login3 failed req:' . json_encode($request->email) . ' errors: ' . json_encode($e->getMessage()));
             return $this->respondWithError($e->getMessage(), 404);
         }
         $userId = Auth::id();
@@ -177,7 +132,6 @@ class AuthController extends ParentController
 
     public function recover(Request $request)
     {
-        return;
         if (!$this->reCapchaTest($request->input('token') , $request->ip())) {
             $error[0] = 'תקלה בבדיקת בוטים, נסו שוב בעוד מספר דקות.';
             return $this->respondWithError(['error' => $error], 422);
@@ -191,7 +145,6 @@ class AuthController extends ParentController
 
     public function reset(Request $request)
     {
-        return; // This method is not implemented in the provided code snippet.
         $credentials = $request->only('password', 'password_confirmation', 'token');
         $validator = Validator::make($credentials, [
             'password' => 'bail|required|confirmed|min:8|max:16',
